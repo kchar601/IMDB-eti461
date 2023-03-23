92ad3ee6e83843fe00e0bacea58361cdba174ce7
Keith Charlton <kcc5354@psu.edu>
2023-03-23 13:33:50 -0400

    removed comments

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ app.use(express.static('public'))
 app.get('/getDB', function(req,res){
  res.setHeader('Content-Type', 'application/json');
 const { MongoClient, ServerApiVersion } = require("mongodb");
-console.log(process.env);
-// Replace the placeholder with your Atlas connection string
 const uri = process.env.uri;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri,  {
         serverApi: {
             version: ServerApiVersion.v1,
@@ -24,9 +21,7 @@ const client = new MongoClient(uri,  {
 );
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     const dbo = client.db("movies");
     await dbo.command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -34,16 +29,11 @@ async function run() {
     console.log(JSON.stringify(items));
     res.json(items);
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
 run().catch(console.dir);
 })
-
-app.get('/message', (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
 
 app.listen(port, () => {
 
