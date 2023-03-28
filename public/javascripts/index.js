ef5b62237b11fa63ac16dc89b6be4ce012d5dc44
Keith Charlton <kcc5354@psu.edu>
2023-03-28 14:49:09 -0400

    class 3/28
    a lot -> login page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,11 @@
 function loadData(db){
-    $('#billboard').empty();
-    const billboardPics = [];
     for (let i = 0; i<db.length; i++){
         const oneLine = db[i];
-        if(oneLine.billboard==1){
-            billboardPics.push(oneLine.poster);
+        if(oneLine.id=="3"){
+            $('#trend1').attr('src', oneLine.img);
             console.log(oneLine.title);
         }
     }
-        for (let i = 0; i<billboardPics.length; i++){
-            console.log(billboardPics[i])
-            document.getElementById("carousel" + i).src = billboardPics[i];
-        }
 }
 function printDB(db){
     for (let i = 0; i<db.length; i++){
