f67a4b28c4ec36ec2bc42ee2fcfd7e019935a885
Will Marsh <wem5115@psu.edu>
2023-03-29 11:12:17 -0400

    Updated buttons and added session logging in comments of app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const bcrypt = require('bcrypt');
 const express = require('express');
 const { fstat } = require('fs');
 const path = require('path');
@@ -96,3 +100,74 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 
 })
+
+/* This is what ChatGPT spit out for the sessions using mongoDb, node and express. We just have to connect the database
+
+
+// connect to mongo database
+mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// create user schema and model
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+// create express app
+const app = express();
+
+// set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set up session
+app.use(session({
+  secret: 'myappsecret',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 3600000 } // 1 hour
+}));
+
+// handle login request
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  // check if user exists in database
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(401).send('Invalid email or password');
+    return;
+  }
+
+  // check if password is correct
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    res.status(401).send('Invalid email or password');
+    return;
+  }
+
+  // start session
+  req.session.user = user;
+  res.cookie('myappsession', req.session.id, { maxAge: 3600000 }); // set session id as cookie
+
+  res.send('Logged in');
+});
+
+// handle logout request
+app.post('/logout', (req, res) => {
+  req.session.destroy();
+  res.clearCookie('myappsession');
+  res.send('Logged out');
+});
+
+// start server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+
+
+
+*/
